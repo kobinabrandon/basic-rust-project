ac6041ecfbd57dd0c7f89016e15a011ddd514cc5
Kobina Brandon <kobinabrandon@proton.me>
2024-11-19 20:39:42 +0000

    Learned about manual error handling using the match statement, and got an introduction to asynchronous code and .await

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::str::Bytes;
-use reqwest::{Response, Error};
+use reqwest::Response;
 
 
 fn main() -> anyhow::Result<()> {
@@ -7,18 +7,28 @@ fn main() -> anyhow::Result<()> {
 }
 
 
-fn make_request(url: &str) -> Result<Response, Box<dyn std::error::Error>> {
-    let response = reqwest::get(url)?;
-    Ok(())
+async fn make_request(url: &str) -> Result<Response, reqwest::Error> {
+     
+    match reqwest::get(url).await {
+        Ok(payload) => {
+            log::info!("Got a bite!");
+            return Ok(payload);
+        },
+        Err(fault) => {
+            log::error!("Something went wrong with the download {}", fault);
+            return Err(fault);
+        }
+    };
+
 }
 
-
+
 fn download_raw_data() -> anyhow::Result<()> {
 
     let url: &str = "https://raw.githubusercontent/selva86/datasets/master/BostonHousing.csv";       
     log::info!("Downloading .csv file");
 
-    let payload = make_request(url=url);  
+    let payload = make_request(url);  
     let bytes: Bytes = payload.bytes()?;    
     
     log::info!("Saving data to disk");
