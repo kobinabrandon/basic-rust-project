3a8e8b1f2204200ed1c41b3edaa6bbb05a8b6205
Kobina Brandon <kobinabrandon@proton.me>
2024-11-19 20:03:37 +0000

    Learning that only the Result and Option types implement the Try trait. The HTTP request doesn't return a Result or Option object

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::str::Bytes;
-use reqwest::Response;
+use reqwest::{Response, Error};
 
 
 fn main() -> anyhow::Result<()> {
    download_raw_data() 
 }
 
+
+fn make_request(url: &str) -> Result<Response, Box<dyn std::error::Error>> {
+    let response = reqwest::get(url)?;
+    Ok(())
+}
+
+
 fn download_raw_data() -> anyhow::Result<()> {
 
-    use std::error::Error;
-    let url: &str = "https://raw.githubusercontent/selva86/datasets/master/BostonHousing.csv";    
-    
+    let url: &str = "https://raw.githubusercontent/selva86/datasets/master/BostonHousing.csv";       
     log::info!("Downloading .csv file");
 
-    let response: Result<Response, Error> = reqwest::get(url)?;  
-    let bytes: Bytes = response.bytes()?;    
+    let payload = make_request(url=url);  
+    let bytes: Bytes = payload.bytes()?;    
     
     log::info!("Saving data to disk");
     std::fs::write("/data", bytes);
